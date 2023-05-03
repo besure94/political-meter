9c4e07c5320f6ea776c8c5966691ac7f4ecbee71
Brian Scherner <brianscherner@gmail.com>
2023-05-03 15:09:54 -0700

    Add function hideResults and get id's for each div class. Set method for each attribute. Add window.onload function and declare hideResults function.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function hideResults() {
+	document.getElementById("liberal").setAttribute("class", "hidden");
+	document.getElementById("moderate").setAttribute("class", "hidden");
+	document.getElementById("conservative").setAttribute("class", "hidden");
+}
+
+window.onload = function() {
+	hideResults();
+
+	document.querySelector("form")
+}
