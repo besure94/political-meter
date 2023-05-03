c058e41e9b500a7f1c5967bd8bec5f1ccda8ea37
Brian Scherner <brianscherner@gmail.com>
2023-05-03 15:49:51 -0700

    Add preventDefault and declare hideResults function. Define variables for all ten questions from HTML file. Make variables use method to get value from 'input' attributes.

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,19 @@ function hideResults() {
 window.onload = function() {
 	hideResults();
 
-	document.querySelector("form")
+	document.querySelector("form").onsubmit = function(event) {
+		event.preventDefault();
+		hideResults();
+		const abortion = document.querySelector("input#abortion").value;
+		const gay = document.querySelector("input#gay").value;
+		const police = document.querySelector("input#police").value;
+		const capitalism = document.querySelector("input#capitalism").value;
+		const healthcare = document.querySelector("input#healthcare").value;
+		const climate = document.querySelector("input#climate").value;
+		const government = document.querySelector("input#government").value;
+		const taxes = document.querySelector("input#taxes").value;
+		const rights = document.querySelector("input#rights").value;
+		const whites = document.querySelector("input#whites").value;
+
+	}
 }
