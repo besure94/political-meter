900420e6d3416d9122f051c21fa97d464bee3852
Brian Scherner <brianscherner@gmail.com>
2023-05-04 14:57:16 -0700

    Declare variables for 'yes' and 'no'. Add method making input variables to lower case. Comment out 7 of 10 variables and old else if expression in order to debug code. Code now works! Will revisit later to fix 'or' statement in 'else if' expression.

--- js/scripts.js
+++ js/scripts.js
@@ -2,36 +2,47 @@ function hideResults() {
 	document.getElementById("liberal").setAttribute("class", "hidden");
 	document.getElementById("moderate").setAttribute("class", "hidden");
 	document.getElementById("conservative").setAttribute("class", "hidden");
+	document.getElementById("undetermined").setAttribute("class", "hidden");
 }
 
 window.onload = function() {
-	hideResults();
 
 	document.querySelector("form").onsubmit = function(event) {
 		event.preventDefault();
 		hideResults();
-		const abortion = document.querySelector("input#abortion").value;
-		const gay = document.querySelector("input#gay").value;
-		const police = document.querySelector("input#police").value;
-		const capitalism = document.querySelector("input#capitalism").value;
-		const healthcare = document.querySelector("input#healthcare").value;
-		const climate = document.querySelector("input#climate").value;
-		const government = document.querySelector("input#government").value;
-		const taxes = document.querySelector("input#taxes").value;
-		const rights = document.querySelector("input#rights").value;
-		const whites = document.querySelector("input#whites").value;
+		const yes = "yes";
+		const no = "no";
+		const abortion = document.querySelector("input#abortion").value.toLowerCase();
+		const gay = document.querySelector("input#gay").value.toLowerCase();
+		const police = document.querySelector("input#police").value.toLowerCase();
+		// const capitalism = document.querySelector("input#capitalism").value.toLowerCase();
+		// const healthcare = document.querySelector("input#healthcare").value.toLowerCase();
+		// const climate = document.querySelector("input#climate").value.toLowerCase();
+		// const government = document.querySelector("input#government").value.toLowerCase();
+		// const taxes = document.querySelector("input#taxes").value.toLowerCase();
+		// const rights = document.querySelector("input#rights").value.toLowerCase();
+		// const whites = document.querySelector("input#whites").value.toLowerCase();
 
-		if (abortion && gay && police && capitalism && healthcare && climate && government && taxes && rights && whites){
-			if (abortion === yes && gay === yes && police === yes && capitalism === no && healthcare === yes && climate === yes && government === yes && taxes === yes && rights === yes && whites === yes);
-				document.getElementById("liberal").removeAttribute("class");
-			} else if (abortion === yes && gay === yes && police === no && capitalism === yes && healthcare === no && climate === yes && government === yes && taxes === yes && rights === no && whites === no) {
-				document.getElementById("liberal").removeAttribute("class");
-			}	else if (abortion === yes && gay === yes && police === undecided && capitalism === undecided && healthcare === undecided && climate === undecided && government === undecided && taxes === undecided && rights === undecided && whites === undecided || abortion === yes && gay === yes && police === undecided && capitalism === yes && healthcare === no && climate === no && government === yes && taxes === yes && rights === no && whites === no) {
-				document.getElementById("moderate").removeAttribute("class");
-			} else if (abortion === no && gay === no && police === no && capitalism === yes && healthcare === no && climate === no && government === no && taxes === no && rights === no && whites === no) {
-				document.getElementById("conservative").removeAttribute("class");
-			} else {
-				document.getElementById("undetermined").removeAttribute("class");
+			if (abortion && gay && police) {
+				if (abortion === yes && gay === yes && police === yes) {
+					document.getElementById("liberal").removeAttribute("class");
+				} else if (abortion === yes && gay === yes && police === no || abortion === no && gay === no && police === yes){
+					document.getElementById("moderate").removeAttribute("class");
+				} else if (abortion === no && gay === no && police === no){
+					document.getElementById("conservative").removeAttribute("class");
+				} else {
+					document.getElementById("undetermined").removeAttribute("class");
+				}
 			}
+		// if (abortion && gay && police && capitalism && healthcare && climate && government && taxes && rights && whites){
+		// 	if (abortion === yes && gay === yes && police === yes && capitalism === no && healthcare === yes && climate === yes && government === yes && taxes === yes && rights === yes && whites === yes)
+		// 		document.getElementById("liberal").removeAttribute("class");
+		// 	}	else if (abortion === yes && gay === yes && police === yes && capitalism === yes && healthcare === yes && climate === no && government === no && taxes === no && rights === no && whites === no || abortion === no && gay === no && police === no && capitalism === no && healthcare === no && climate === yes && government === yes && taxes === yes && rights === yes && whites === yes) {
+		// 		document.getElementById("moderate").removeAttribute("class");
+		// 	} else if (abortion === no && gay === no && police === no && capitalism === yes && healthcare === no && climate === no && government === no && taxes === no && rights === no && whites === no) {
+		// 		document.getElementById("conservative").removeAttribute("class");
+		// 	} else {
+		// 		document.getElementById("undetermined").removeAttribute("class");
+		// 	}
 		};
 	};
