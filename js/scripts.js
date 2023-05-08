0d4b913a7e387f896b5db925dca73c22a9f804bb
Brian Scherner <brianscherner@gmail.com>
2023-05-08 09:20:15 -0700

    Delete commented out code.

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,9 @@
 // }
 
 window.addEventListener("load", function() {
-	const form = document.querySelector("form");
-	const resetBtn = document.getElementById("reset");
-	const liberal = document.getElementById("liberal");
-	// const moderate = document.getElementById("moderate");
-	// let conservative = document.getElementById("conservative");
-	// let undetermined = document.getElementById("undetermined");
+	let form = document.querySelector("form");
+	let resetBtn = document.getElementById("reset");
+	let liberal = document.getElementById("liberal");
 
 	form.addEventListener("submit", function(event) {
 		const yes = "yes";
@@ -21,35 +18,20 @@ window.addEventListener("load", function() {
 		const police = document.getElementById("police").value.toLowerCase();
 
 		liberal.removeAttribute("class");
-		// moderate.removeAttribute("class");
-		// conservative.removeAttribute("class");
-		// undetermined.removeAttribute("class");
 		event.preventDefault();
 
 		if (abortion && gay && police) {
 			if (abortion === yes && gay === yes && police === yes) {
 				document.getElementById("liberal").removeAttribute("class");
 			} else if (abortion === yes && gay === yes && police === no || abortion === no && gay === no && police === yes){
-				// document.getElementById("moderate").removeAttribute("class");
+				document.getElementById("moderate").removeAttribute("class");
 			} else if (abortion === no && gay === no && police === no){
-				// document.getElementById("conservative").removeAttribute("class");
+				document.getElementById("conservative").removeAttribute("class");
 			} else {
-				// document.getElementById("undetermined").removeAttribute("class");
+				document.getElementById("undetermined").removeAttribute("class");
 			}
 		}
 	});
-
-		// if (abortion && gay && police) {
-		// 	if (abortion === yes && gay === yes && police === yes) {
-		// 		document.getElementById("liberal").removeAttribute("class");
-		// 	} else if (abortion === yes && gay === yes && police === no || abortion === no && gay === no && police === yes){
-		// 		document.getElementById("moderate").removeAttribute("class");
-		// 	} else if (abortion === no && gay === no && police === no){
-		// 		document.getElementById("conservative").removeAttribute("class");
-		// 	} else {
-		// 		document.getElementById("undetermined").removeAttribute("class");
-		// 	}
-		// }
 
 	form.addEventListener("submit", function() {
 		resetBtn.removeAttribute("class");
@@ -61,9 +43,9 @@ window.addEventListener("load", function() {
 
 	resetBtn.addEventListener("click", function() {
 		liberal.setAttribute("class", "hidden");
-		// moderate.setAttribute("class", "hidden");
-		// conservative.setAttribute("class", "hidden");
-		// undetermined.setAttribute("class", "hidden");
+		moderate.setAttribute("class", "hidden");
+		conservative.setAttribute("class", "hidden");
+		undetermined.setAttribute("class", "hidden");
 		document.getElementById("abortion").value = null;
 		document.getElementById("gay").value = null;
 		document.getElementById("police").value = null;
